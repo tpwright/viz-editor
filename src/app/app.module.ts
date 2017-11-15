6eb7ebc2a9751fe36812d3df026a6aeeafe7fd46
Tom Wright <tpwright4423@gmail.com>
2017-11-15 12:10:59 -0500

    Interim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { MatTabsModule }            from '@angular/material';
+import { MatIconModule }            from '@angular/material/icon';
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent }             from './app.component';
+import { MenuComponent }            from './menu/menu.component';
+import { LayoutTabsComponent }      from './layout-tabs/layout-tabs.component';
+import { StatusItemListComponent }  from './status-item-list/status-item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LayoutTabsComponent,
+    StatusItemListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,            // The order of these imports is critical
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

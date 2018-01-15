5b1544c4e3133db185fe4ca9e073a41ad4736718
Anthony Dimuzio <anthonydimuzio@qcsoftware.com>
2018-01-15 12:19:29 -0500

    saving settings

--- src/app/edit-settings/edit-settings.component.ts
+++ src/app/edit-settings/edit-settings.component.ts
@@ -37,37 +37,10 @@ export class EditSettingsComponent
       }});
     //what the heck rArray isn't the expected value at all.
     let rVal = data.value
-      rVal.displayableProperties = this.settingsList;//this is stupid
+      rVal.displayableProperties = this.settingsList;//todo: fix the settings object
       
     this.dialogRef.close(rVal);
   }
-
-
-  // For reference
- 
-  // customValidationElements: ITdDynamicElementConfig[] = [{
-  //   name: 'evenElement',
-  //   label: 'Even Number',
-  //   type: TdDynamicType.Number,
-  //   validators: [{
-  //     validator: (control: AbstractControl) => {
-  //       let isValid: boolean = (!control.value && control.value !== 0) || ((control.value % 2 ) === 0);
-  //       return !isValid ? {even: true} : undefined;
-  //     },
-  //   }],
-  // }, {
-  //   name: 'oddInRangeElement',
-  //   label: 'Odd Number Between 8 and 20',
-  //   type: TdDynamicType.Number,
-  //   min: 8,
-  //   max: 20,
-  //   validators: [{
-  //     validator: (control: AbstractControl) => {
-  //       let isValid: boolean = (!control.value && control.value !== 0) || ((control.value % 2) !== 0);
-  //       return !isValid ? {odd: true} : undefined;
-  //     },
-  //   }],
-  // }];
 
   formatConfig(obj: {name: string, value: string | number | boolean}[]) : ITdDynamicElementConfig[]{
     let rVal : ITdDynamicElementConfig[] = [];
